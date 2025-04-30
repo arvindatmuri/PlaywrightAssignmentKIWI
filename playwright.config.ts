7831bf04c9685bf78583863a604a3f0df402d576
Arvind Atmuri <arvindatmuri@gmail.com>
2025-04-30 18:59:19 +1000

    Configured Base Url From Environment File

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
-
+import dotenv from 'dotenv';
+import path from 'path';
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -11,6 +12,10 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
+const environment = process.env.TEST_ENV ? `${process.env.TEST_ENV}.env` : "dev.env"
+dotenv.config({ path: path.resolve(__dirname, "./env", environment) });
+
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -26,8 +31,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
+    baseURL: process.env.BASE_URL ? process.env.BASE_URL : "https://www.westpac.co.nz/kiwisaver-investments/kiwisaver/kiwisaver-calculators/kiwisaver-calculator/",
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
