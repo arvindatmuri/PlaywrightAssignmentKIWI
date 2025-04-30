cdd5c5300aad3847725749289297efae3c1a2686
Arvind Atmuri <arvindatmuri@gmail.com>
2025-04-30 19:07:36 +1000

    Create HomePage.ts

---
+++ tests/pageObjects/HomePage.ts
@@ -0,0 +1,12 @@
+import { expect, Locator, Page } from "@playwright/test"
+
+
+export class HomePage {
+    
+    readonly page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+
+    }
+}
